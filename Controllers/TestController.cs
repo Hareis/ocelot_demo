2a33aabbd4b15c4c05e3b725bedd4907fd54fef6
hareis <hareis@163.com>
2020-09-29 21:14:32 +0800

    添加项目文件。

---
+++ Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aj.microService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Object GetTestContent()
+        {
+            return new
+            {
+                api = "base",
+                dates = Enumerable.Range(1, 5).Select(index => DateTime.Now.AddDays(index)).ToArray(),
+            };
+        }
+    }
+}
